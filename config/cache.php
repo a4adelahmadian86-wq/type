5ddcfb4c7298c8a22d9cf9d3605435bf7abc5979
a4adelahmadian86-wq <a4.adelahmadian86@gmail.com>
2026-09-12 18:04:29 +0330

    Add cache configuration

---
+++ config/cache.php
@@ -0,0 +1,2 @@
+<?php
+return ['default'=>env('CACHE_STORE','database'),'stores'=>['database'=>['driver'=>'database','table'=>'cache','connection'=>null,'lock_connection'=>null],'array'=>['driver'=>'array','serialize'=>false]],'prefix'=>env('CACHE_PREFIX','farast_cache')];
