e18a238986e6f2e9c307ca5d32ecc0a91e5b11d0
a4adelahmadian86-wq <a4.adelahmadian86@gmail.com>
2026-09-12 18:05:09 +0330

    Add safe logging configuration

---
+++ config/logging.php
@@ -0,0 +1,2 @@
+<?php
+return ['default'=>env('LOG_CHANNEL','stack'),'deprecations'=>['channel'=>env('LOG_DEPRECATIONS_CHANNEL','null'),'trace'=>false],'channels'=>['stack'=>['driver'=>'stack','channels'=>['daily'],'ignore_exceptions'=>false],'daily'=>['driver'=>'daily','path'=>storage_path('logs/laravel.log'),'level'=>env('LOG_LEVEL','warning'),'days'=>14],'stderr'=>['driver'=>'monolog','level'=>env('LOG_LEVEL','warning'),'handler'=>Monolog\Handler\StreamHandler::class,'with'=>['stream'=>'php://stderr']],'null'=>['driver'=>'monolog','handler'=>Monolog\Handler\NullHandler::class]]];
