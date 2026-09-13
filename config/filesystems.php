fe0ef512e1ace020c975ecd423db0b65a975501c
a4adelahmadian86-wq <a4.adelahmadian86@gmail.com>
2026-09-13 19:56:27 +0330

    fix private storage disk configuration

--- config/filesystems.php
+++ config/filesystems.php
@@ -1,2 +1,31 @@
 <?php
-return ['default'=>env('FILESYSTEM_DISK','local'),'disks'=>['local'=>['driver'=>'local','root'=>storage_path('app/private'),'throw'=>false],'public'=>['driver'=>'local','root'=>storage_path('app/public'),'url'=>env('APP_URL').'/storage','visibility'=>'public','throw'=>false]],'links'=>[public_path('storage')=>storage_path('app/public')]];
+
+return [
+    'default' => env('FILESYSTEM_DISK', 'local'),
+
+    'disks' => [
+        'local' => [
+            'driver' => 'local',
+            'root' => storage_path('app/private'),
+            'throw' => false,
+        ],
+
+        'private' => [
+            'driver' => 'local',
+            'root' => storage_path('app/private'),
+            'throw' => false,
+        ],
+
+        'public' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
+    ],
+
+    'links' => [
+        public_path('storage') => storage_path('app/public'),
+    ],
+];
