815b827e383dc135611905f9c14dc02bfdace626
a4adelahmadian86-wq <a4.adelahmadian86@gmail.com>
2026-09-14 10:37:51 +0330

    feat: schedule two-tier user file retention

--- routes/console.php
+++ routes/console.php
@@ -1,3 +1,6 @@
 <?php
+
 use Illuminate\Support\Facades\Schedule;
+
 Schedule::command('farast:purge-files')->daily();
+Schedule::command('farast:prune-user-files')->dailyAt('03:20')->withoutOverlapping();
