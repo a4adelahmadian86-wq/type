b81ae41159894bc5e7bd81d5f2a4ffb02402d504
a4adelahmadian86-wq <a4.adelahmadian86@gmail.com>
2026-09-16 09:33:13 +0330

    Fix microphone resampling to 16 kHz PCM for Google streaming

--- public/js/farast-voice-worklet.js
+++ public/js/farast-voice-worklet.js
@@ -1,8 +1,13 @@
 class FarastPcmProcessor extends AudioWorkletProcessor {
   constructor() {
     super();
+    this.inputRate = sampleRate;
+    this.outputRate = 16000;
+    this.ratio = this.inputRate / this.outputRate;
     this.buffer = [];
+    this.position = 0;
     this.targetSamples = 1600;
+    this.output = [];
   }
 
   process(inputs) {
@@ -11,14 +16,27 @@ class FarastPcmProcessor extends AudioWorkletProcessor {
 
     for (let i = 0; i < input.length; i++) this.buffer.push(input[i]);
 
-    while (this.buffer.length >= this.targetSamples) {
-      const samples = this.buffer.splice(0, this.targetSamples);
-      const pcm = new Int16Array(samples.length);
-      for (let i = 0; i < samples.length; i++) {
-        const sample = Math.max(-1, Math.min(1, samples[i]));
-        pcm[i] = sample < 0 ? sample * 32768 : sample * 32767;
+    while (this.position + 1 < this.buffer.length) {
+      const left = Math.floor(this.position);
+      const fraction = this.position - left;
+      this.output.push(this.buffer[left] * (1 - fraction) + this.buffer[left + 1] * fraction);
+      this.position += this.ratio;
+
+      if (this.output.length >= this.targetSamples) {
+        const pcm = new Int16Array(this.output.length);
+        for (let i = 0; i < this.output.length; i++) {
+          const sample = Math.max(-1, Math.min(1, this.output[i]));
+          pcm[i] = sample < 0 ? sample * 32768 : sample * 32767;
+        }
+        this.port.postMessage(pcm.buffer, [pcm.buffer]);
+        this.output = [];
       }
-      this.port.postMessage(pcm.buffer, [pcm.buffer]);
+    }
+
+    const remove = Math.floor(this.position);
+    if (remove > 0) {
+      this.buffer.splice(0, remove);
+      this.position -= remove;
     }
 
     return true;
