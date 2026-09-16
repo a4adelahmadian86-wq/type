d5bd219dfc06dd056659daa8ca3e4d1591724770
a4adelahmadian86-wq <a4.adelahmadian86@gmail.com>
2026-09-16 08:03:28 +0330

    feat: register capability middleware alias

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'single.editor' => App\Http\Middleware\EnsureSingleEditor::class,
             'admin' => App\Http\Middleware\AdminOnly::class,
+            'capability' => App\Http\Middleware\EnsureCapability::class,
         ]);
         $middleware->append(App\Http\Middleware\EnsurePrivateStorageDisk::class);
     })
